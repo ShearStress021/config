4792a83635a09ee73760ea8f9bf242a9818f27a7
shearstress021 <shearstress021@gmail.com>
2025-10-20 22:19:57 -0700

    my vimrc bash setup

--- my_setup.sh
+++ my_setup.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+sudo apt remove vim-tiny vim
 
-myDep=("git", "curl", "build_essential","cmake","python3-dev")
+
+myDep=("flatpak" "gnome-software-plugin-flatpak" "gnome-tweaks" "git" "curl" "build-essential" "cmake" "python3-dev" "vim-nox")
+
 
 sudo apt update && sudo apt upgrade
 for item in ${myDep[@]}; do
-	sudo apt update && sudo apt install $item
+	var=$(dpkg -l $item| grep 'ii' | awk "{print $2}")
+
+	if [[ $item != $var  ]]; then
+		
+		sudo apt update && sudo apt install $item
+	else 
+		:
+	fi
 
 done
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cd ~/.vim/bundle/YouCompleteMe
+
+python3 install.py --clangd-completer
+
